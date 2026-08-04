7ffb9a09596f81854459f88eecf6c63dea686e6d
羅子淇 Jacky Law, F.I.H.R.M.(HK), FHKIoD <289607996+jackylawck@users.noreply.github.com>
2026-08-04 18:05:17 +0800

    Fix formatting and remove commented line in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,3 @@
-// js/config.js
-
 export const APP_CONFIG = {
   DEFAULT_LANG: 'zh',
   STORAGE_KEY_LANG: 'eat_spin_lang',
@@ -7,10 +5,9 @@ export const APP_CONFIG = {
     HKO: {
       WEATHER_URL: 'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=tc',
       WARNING_URL: 'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=warnsum&lang=tc',
-      TIMEOUT: 4000 // 4秒超時
+      TIMEOUT: 4000
     },
     KMB: {
-      BASE_URL: 'https://data.etagmb.gov.hk',
       TIMEOUT: 3000
     }
   }
